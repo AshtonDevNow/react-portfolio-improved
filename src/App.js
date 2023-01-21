f1a4565b00a21f0a7a1b27657106b0acaa3ce487
Ashton Bunn <ashtonwork@hotmail.com>
2023-01-22 06:02:59 +0800

    Added sidebar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg'
 import './App.scss'
+import { Route, Routes } from 'react-router-dom'
+// import Home from './components/Home'
+import Layout from './components/Layout'
+// import About from './components/About'
+// import Contact from './components/Contact'
+// import Projects from './pages/Projects'
+// import ProjectDisplay from './pages/ProjectDisplay'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Projects />} />
+          <Route path="/project/:id" element={<ProjectDisplay />} />
+          <Route path="/contact" element={<Contact />} /> */}
+        </Route>
+      </Routes>
+    </>
   )
 }
 
