1cee1f678bfe9c97e8d25323d9fb86c4ac24a518
Ashton Bunn <ashtonwork@hotmail.com>
2023-01-23 06:21:10 +0800

    The projects section

---
+++ src/assets/helpers/ProjectList.js
@@ -0,0 +1,33 @@
+import BoredomBakeryLogo from '../images/bordembakery.png'
+import GitTheKit from '../images/gitthekit.png'
+import GiggleLogo from '../images/giggle.png'
+
+export const ProjectList = [
+  {
+    name: 'Boredom Bakery',
+    image: BoredomBakeryLogo,
+    skills: 'JS, HTML, CSS',
+    description:
+      'I was hired to make a website to display this bakeries products.  I used this as an opportunity to practice vanilla JS. ',
+    website: 'https://ashtondevnow.github.io/bbakery/',
+    github: 'https://github.com/AshtonDevNow/react-portfolio',
+  },
+  {
+    name: 'Giggle',
+    image: GiggleLogo,
+    skills: 'Javascript, Ruby on Rails and Wechat Devtools',
+    description:
+      'In China one of the most used applications is wechat mini-programs, which launches application via the cloud.  This was my groups final project during my full-stack bootcamp with Le Wagon.  This app is a market place for individuals to find side work or find or to recruit individuals.  For this project I focused on front-end.',
+    website: 'Can only be accessed',
+    github: 'https://github.com/rmegarbane/Gigfinder',
+  },
+  {
+    name: 'Git The Kit',
+    image: GitTheKit,
+    skills: 'Javascript, Ruby on Rails and Wechat Devtools',
+    description:
+      'In China one of the most used applications is wechat mini-programs, which launches application via the cloud.  This application on wechat min-programs was marketplace for individuals to rent or rent out others sporting equipment.  This was built as my first project during my Fullstack Bootcamp with Le Wagon in Shanghai.  For this particular project I focused on the back-end, using Ruby on Rails to link product post and requests to accounts.',
+    website: 'Can only be accessed in China',
+    github: 'https://github.com/AshtonDevNow/equiprent-backend2',
+  },
+]
